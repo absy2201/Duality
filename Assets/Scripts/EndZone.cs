8555fc45b31e657ed2c6ce9f8ab3e1ed1dae330a
absy_2201 <abhimanyu.chattopadhyay@gmail.com>
2022-01-28 12:34:16 +0000

    Added VFX, Textures and Scenes

---
+++ Assets/Scripts/EndZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            int buildIndex = SceneManager.GetActiveScene().buildIndex;
+            if(SceneManager.GetSceneByBuildIndex(buildIndex+1) != null)
+            {
+                SceneManager.LoadScene(buildIndex+1);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(() => Play());
+        quitButton.onClick.AddListener(() => Quit());
+    }
+
+    private void Play()
+    {
+        int buildIndex = SceneManager.GetActiveScene().buildIndex;
+        if (SceneManager.GetSceneByBuildIndex(buildIndex + 1) != null)
+        {
+            SceneManager.LoadScene(buildIndex + 1);
+        }
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
